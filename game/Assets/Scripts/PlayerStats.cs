1b80c4ad74725936d535a424e6552d9df9a4a182
Tomki2258 <52979692+Tomki2258@users.noreply.github.com>
2020-05-26 15:11:53 +0200

    Paski statystyk+ulepszenie XP

--- game/Assets/Scripts/PlayerStats.cs
+++ game/Assets/Scripts/PlayerStats.cs
@@ -1,28 +1,107 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
-    public int XP=0;
-    public int thirst=100;
-    public int HP = 100;
-    public int stamina = 100;
 
-   
-    void Start()
+    public float Health;
+    public float healthOverTimer;
+
+    public float Stamina;
+    public float staminaOverTime;
+
+    public float Hunger;
+    public float hungerOverTime;
+
+    public float Thirst;
+    public float thirstOverTime;
+
+    public float Radiation;
+    public float radiationOverTime;
+
+
+
+    public Slider HealthBar;
+    public Slider StaminaBar;
+    public Slider HungerBar;
+    public Slider ThirstBar;
+    public Slider RadiationBar;
+
+    public float minAmount = 5f;
+    public float sprintSpeed = 5f;
+
+    Rigidbody myBody;
+
+    private void Start()
     {
-        
+        myBody = GetComponent<Rigidbody>();
+        HealthBar.maxValue = Health;
+        StaminaBar.maxValue = Stamina;
+        HungerBar.maxValue = Hunger;
+        ThirstBar.maxValue = Thirst;
+        RadiationBar.maxValue = Radiation;
+
+        updateUI();
     }
 
-    
-    void Update()
+    private void Update()
     {
-        
+        CalculateValues();
     }
-    //aby dodać XP wpisujemy w skrypcie addXP(ilość);
-    public void addXP(int addedXP)
+
+    private void CalculateValues()
     {
-        XP = XP + addedXP;
+        Hunger -= hungerOverTime * Time.deltaTime;
+        Thirst -= thirstOverTime * Time.deltaTime;
+
+        if (Hunger <= minAmount || Thirst <= minAmount)
+        {
+            Health -= healthOverTimer * Time.deltaTime;
+            Stamina -= staminaOverTime * Time.deltaTime;
+        }
+
+        if (myBody.velocity.magnitude >= sprintSpeed && myBody.velocity.y == 0)
+        {
+            Stamina -= staminaOverTime * Time.deltaTime;
+            Hunger -= hungerOverTime * Time.deltaTime;
+            Thirst -= thirstOverTime * Time.deltaTime;
+        }
+        else
+        {
+            Stamina += staminaOverTime * Time.deltaTime;
+        }
+
+
+        if (Health <= 0)
+        {
+            print("PLAYER HAS DIED");
+        }
+
+        updateUI();
     }
+
+    private void updateUI()
+    {
+        Health = Mathf.Clamp(Health, 0, 100f);
+        Stamina = Mathf.Clamp(Stamina, 0, 100f);
+        Hunger = Mathf.Clamp(Hunger, 0, 100f);
+        Thirst = Mathf.Clamp(Thirst, 0, 100f);
+        Radiation = Mathf.Clamp(Radiation, 0, 100f);
+
+        HealthBar.value = Health;
+        StaminaBar.value = Stamina;
+        HungerBar.value = Hunger;
+        ThirstBar.value = Thirst;
+        RadiationBar.value = Radiation;
+    }
+
+    public void TakeDamage(float pDam)
+    {
+        Health -= pDam;
+        updateUI();
+    }
+
 }
---
+++ game/Assets/Scripts/XP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XP : MonoBehaviour
+{
+    int xp = 0;
+    int expPoint = 0;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            addXP(50);
+            Debug.Log("xp" + xp);
+            Debug.Log("expieriencePoint" + expPoint);
+        }
+        if (xp == 100)
+        {
+            expPoint++;
+            xp = 0;
+        }
+    }
+
+    public void addXP(int newXP)
+    {
+        xp += newXP;
+    }
+}
